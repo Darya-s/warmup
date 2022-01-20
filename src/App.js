7892b677553219e80c17a843c344cdfc2392ec57
Darya-s <darya.stepanova95@gmail.com>
2022-01-20 16:21:24 +0100

    Added files for warmup exercise

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './stories/Header';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header backgroundColor={backgroundColor}/>
+        
+      
+    </div>
+  );
+}
+
+export default App;
